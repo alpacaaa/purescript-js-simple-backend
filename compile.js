b1d761fb22b70540db63c773c9214bf841c2a9f5
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-01-15 00:34:10 +0100

    Move out stuff into /lib.

--- compile.js
+++ compile.js
@@ -27,7 +27,8 @@ const compileDeclGroup = id => group => {
 const compileExpr = id => expr => {
   const [type, ...rest] = expr
   const compiler = compilerForExpr(type)
-  return compiler(id)(rest)
+  const context = { id, compileExpr, }
+  return compiler(context)(rest)
 }
 
 const compilerForExpr = type => {
@@ -44,45 +45,7 @@ const compilerForExpr = type => {
 
 
 const compilers = {
-  Literal: id => ([literal]) => {
-    const [type, fields] = literal
-
-    if (type === 'ObjectLiteral') {
-      const obj = Object.keys(fields)
-      .map(k => {
-        return k + ': ' + compileExpr(id)(fields[k]) + ','
-      })
-      .join("\n")
-
-      return ['{', obj, '}'].join("\n")
-    }
-
-    if (type === 'ArrayLiteral') {
-      const arr = fields
-      .map(compileExpr(id))
-      .join(", ")
-
-      return ['[', arr, ']'].join("\n")
-    }
-
-    if (
-      type === 'IntLiteral'
-      || type === 'NumberLiteral'
-    ) {
-      return fields
-    }
-
-    if (
-      type === 'StringLiteral'
-      || type === 'CharLiteral'
-    ) {
-      return '"' + fields + '"'
-    }
-
-    if (type === 'BooleanLiteral') {
-      return fields ? 'true' : 'false'
-    }
-  }
+  Literal: require('./lib/literal'),
 }
 
 
